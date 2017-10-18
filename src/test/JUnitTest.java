be8a263ef666637f0ab1b179f1a46144075b36b0
peck liu <xiaoyang20170204@gmail.com>
2017-10-18 17:48:12 +0800

    refactor step1

--- src/test/JUnitTest.java
+++ src/test/JUnitTest.java
@@ -4,11 +4,23 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class JUnitTest {
+import chapter_one.Customer;
+import chapter_one.Movie;
+import chapter_one.Rental;
+import junit.framework.TestCase;
+
+public class JUnitTest extends TestCase{
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Movie movie = new Movie("New_Release_Movie", 1);
+		Rental rental = new Rental(movie, 2);
+		Customer customer = new Customer("peck");
+		customer.addRental(rental);
+		String result = customer.statement();
+		assertEquals(result , "Rental Record for	peck\n" + 
+				"	New_Release_Movie	6.0\n" + 
+				"Amount owed is 6.0\n" + 
+				"You earned 2 frequent renter points");
 	}
-
 }
